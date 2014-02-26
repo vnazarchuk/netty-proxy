e8b2c23c5d2d6c4433892de1ea49c215c431576b
Valeriy Nazarchuk <vnazarchuk@collective.com>
2014-02-26 18:29:08 -0500

    added port forwarding test

--- src/test/java/com/collective/delayedproxy/RedisTest.java
+++ src/test/java/com/collective/delayedproxy/RedisTest.java
@@ -6,8 +6,9 @@ import org.junit.Ignore;
 import org.junit.Test;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.exceptions.JedisConnectionException;
 
-import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.*;
 
 public class RedisTest {
 
@@ -39,5 +40,40 @@ public class RedisTest {
         JedisPool pool = new JedisPool(DelayedProxyTest.REMOTE_HOST, DelayedProxyTest.LOCAL_PORT);
         Jedis jedis = pool.getResource();
         assertNotNull(jedis);
+    }
+
+    @Test
+    public void testPortForwarding() {
+        JedisPool remotePool = new JedisPool(DelayedProxyTest.REMOTE_HOST, DelayedProxyTest.REMOTE_PORT);
+        JedisPool localPool = new JedisPool(DelayedProxyTest.REMOTE_HOST, DelayedProxyTest.LOCAL_PORT);
+        Jedis remoteJedis = remotePool.getResource();
+        Jedis localJedis = localPool.getResource();
+        try {
+            remoteJedis.set("key1", "value1");
+
+            new DelayedProxy(DelayedProxyTest.LOCAL_PORT, DelayedProxyTest.REMOTE_PORT).start();
+
+            String value = localJedis.get("key1");
+            assertTrue(value.equals("value1"));
+        } catch (JedisConnectionException e) {
+            if (localJedis != null) {
+                localPool.returnBrokenResource(localJedis);
+                localJedis = null;
+            }
+            if (remoteJedis != null) {
+                remotePool.returnBrokenResource(remoteJedis);
+                remoteJedis = null;
+            }
+            e.printStackTrace();
+            fail();
+        } finally {
+            if (remoteJedis != null)
+                remotePool.returnResource(remoteJedis);
+            if (localJedis != null)
+                localPool.returnResource(localJedis);
+            localPool.destroy();
+            remotePool.destroy();
+            redisProcess.destroy();
+        }
     }
 }
